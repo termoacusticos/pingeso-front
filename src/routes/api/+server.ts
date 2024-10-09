b8ce69b33ad8074660a8af0df46a074a838fa3e9
Skyrdow <lucas.mesias@usach.cl>
2024-10-09 18:43:55 -0300

    test: db results

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -4,12 +4,14 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ fetch, request, platform }) => {
 	console.log('==== SERVER ====');
 	let sql = await platform?.env.DB.prepare('SELECT * FROM sqlite_master;').run();
+	console.log(sql?.results);
+	console.log(sql?.meta);
 
 	let body = await request.json();
 	console.log('==== request body ====');
 	console.log(body);
 
-	return json({ adios: sql }, { status: 299, statusText: 'yalo envie' });
+	return json({ adios: sql?.results }, { status: 299, statusText: 'yalo envie' });
 	// equivale a: (el response se agrega implicitamente, y se puede usar lo de abajo también)
 	// return new Response(JSON.stringify({ adios: 'ADIOS' }), {status: 299, statusText: 'yalo envie' })
 };
