ffded793d3b1b2631db9c967e081d9034c27236f
Alexandra Navarro <alexandra.navarro@usach.cl>
2024-11-06 23:08:54 -0300

    add: modificacion page

--- src/routes/home/cotizar/+page.svelte
+++ src/routes/home/cotizar/+page.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
 	import Ventana from '$lib/components/Ventana.svelte';
 
-	let materiales= [
-		{ nombre: "PVC", calidad: "Alta", descripcion1: "Sistema Europeo", descripcion2: "Warm Edge", seleccionado: false },
-		{ nombre: "Aluminio Xelentia", calidad: "Alta", descripcion1: "Sistema Premium", descripcion2: "Warm Edge", seleccionado: false },
-		{ nombre: "Aluminio Estandar", calidad: "Media", descripcion1: "Sistema Tradicional", descripcion2: "Normal", seleccionado: false }
+	let materiales = [
+		{
+			nombre: 'PVC',
+			calidad: 'Alta',
+			descripcion1: 'Sistema Europeo',
+			descripcion2: 'Warm Edge',
+			seleccionado: false
+		},
+		{
+			nombre: 'Aluminio Xelentia',
+			calidad: 'Alta',
+			descripcion1: 'Sistema Premium',
+			descripcion2: 'Warm Edge',
+			seleccionado: false
+		},
+		{
+			nombre: 'Aluminio Estandar',
+			calidad: 'Media',
+			descripcion1: 'Sistema Tradicional',
+			descripcion2: 'Normal',
+			seleccionado: false
+		}
+	];
+
+	let coloresDisponibles = [
+		{ nombre: 'Nogal', seleccionado: false },
+		{ nombre: 'Titanio', seleccionado: false },
+		{ nombre: 'Blanco', seleccionado: false },
+		{ nombre: 'Negro', seleccionado: false },
+		{ nombre: 'Roble Dorado', seleccionado: false },
+		{ nombre: 'Mate', seleccionado: false },
+		{ nombre: 'Antracita', seleccionado: false }
 	];
 
 	let cotizaciones = $state([
@@ -15,7 +43,7 @@
 			alto: '',
 			ancho: '',
 			vidrio: '',
-			precio: '',
+			precio: ''
 		}
 	]);
 	let mostrar_eliminar = $derived(cotizaciones.length > 1);
@@ -23,7 +51,10 @@
 	$inspect(cotizaciones);
 
 	function agregarVentana() {
-		cotizaciones = [...cotizaciones, { tipo: '', cantidad: '' , color: '', alto: '', ancho: '',vidrio: '', precio: '' }];
+		cotizaciones = [
+			...cotizaciones,
+			{ tipo: '', cantidad: '', color: '', alto: '', ancho: '', vidrio: '', precio: '' }
+		];
 	}
 
 	function eliminarVentana(index: any) {
@@ -31,10 +62,11 @@
 	}
 </script>
 
-<div class="min-h-screen ml-14 bg-gray-100 p-6">
+<div class="min-h-screen bg-gray-100 p-6 max-w-screen-lg mx-auto">
 	<h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Crear Cotización de Ventanas</h1>
-	<div class="overflow-auto">
-		<table class="min-w-full bg-white border border-gray-300">
+	<div class="flex justify-center space-x-6">
+		<!-- Seleccionar Materiales -->
+		<table class="min-w-max bg-white border border-gray-300">
 			<thead>
 				<tr>
 					<th class="px-4 py-2 bg-gray-200 text-left font-bold">Material (Hasta 3)</th>
@@ -59,8 +91,29 @@
 				{/each}
 			</tbody>
 		</table>
+		<!-- Seleccionar Color -->
+		<table class="min-w-max bg-white border border-gray-300">
+			<thead>
+				<tr>
+					<th class="px-4 py-2 bg-gray-200 text-left font-bold">Color (Hasta 2 opciones)</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each coloresDisponibles as color}
+					<tr class="border-t">
+						<td class="px-4 py-2">
+							<label class="flex items-center space-x-2">
+								<input type="checkbox" bind:checked={color.seleccionado} />
+								<span>{color.nombre}</span>
+							</label>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 
+	<!-- Ventanas -->
 	<div class="space-y-6 mt-8">
 		{#each cotizaciones as ventana, index}
 			<Ventana {index} {eliminarVentana} bind:ventana={cotizaciones[index]} {mostrar_eliminar} />
