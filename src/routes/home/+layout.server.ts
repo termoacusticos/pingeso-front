8b436cd152c667e7e07f06e4a69994d751efce65
Skyrdow <lucas.mesias@usach.cl>
2025-02-03 11:21:10 -0300

    fix: sidebar admin

--- src/routes/home/+layout.server.ts
+++ src/routes/home/+layout.server.ts
@@ -1,6 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	return { is_admin: locals.session.is_admin };
+	return { is_admin: locals.session?.is_admin };
 };
--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import type { LayoutData } from './$types';
 
 	interface Props {
 		children: Snippet;
-		is_admin: boolean;
+		data: LayoutData;
 	}
 
-	let { children, is_admin }: Props = $props();
+	let { children, data }: Props = $props();
 </script>
 
 <div class="relative flex flex-col w-full bg-gray-100">
 	<!-- <Navbar /> -->
-	<Sidebar {is_admin} />
+	<Sidebar is_admin={data.is_admin} />
 	{@render children()}
 </div>
